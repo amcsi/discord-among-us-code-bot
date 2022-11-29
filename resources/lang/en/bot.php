2eadd56a8af05ee89e6f36f25834427e0f2fc070
Attila Szeremi <attila@szeremi.org>
2022-11-29 12:38:12 +0100

    And English translation and change default language to it

---
+++ resources/lang/en/bot.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'startingMessage' => 'Hello. This is where you will find Among Us lobby codes.',
+    'disconnected' => 'I am currently disconnected and cannot show any Among Us lobby codes.',
+    'howToUpdateCodes' => 'Write your six-digit lobby code (optionally specifying NA or EU) in the <#:sourceChannelId> channel, and the code will automatically appear in this list next to the voice channel the code belong to.',
+    'noCodes' => 'There are currently no Among Us lobby codes.',
+    'hereAreTheCodes' => 'Here are the current Among Us lobby codes:',
+];
